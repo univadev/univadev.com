5a8ea403651f8d6084df6103d1dc57d6d2b8147a
ryanmah10 <ryanmah2@gmail.com>
2025-10-13 22:49:39 -0400

    mvp

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # UnivaDev.com
-
--- script.js
+++ script.js
@@ -3,17 +3,27 @@ const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
+            
     if (scrollTop === 0) {
-        
         navbar.classList.remove('hidden');
     } else if (scrollTop > lastScrollTop) {
-        
         navbar.classList.add('hidden');
-    } else {
-        
-        navbar.classList.remove('hidden');
+     } else {
+         navbar.classList.remove('hidden');
     }
-    
+            
     lastScrollTop = scrollTop;
 });
+
+const mobileMenuToggle = document.getElementById('mobileMenuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+mobileMenuToggle.addEventListener('click', function() {
+    mobileMenu.classList.toggle('active');
+});
+
+function toggleMobileDropdown(event, id) {
+    event.preventDefault();
+    const dropdown = document.getElementById(id);
+        ropdown.classList.toggle('active');
+}
