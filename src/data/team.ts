587d5110245cd4937d3454e7c476ad95621f1815
Eldiiar Bekbolotov <eldiiarbekbolotov@gmail.com>
2025-12-06 21:47:24 -0500

    .

--- src/data/team.ts
+++ src/data/team.ts
@@ -14,7 +14,7 @@ export const team: TeamMember[] = [
     role: "Chief Executive Officer",
     image: "/abt.png",
     bio: "Eldiiar Bekbolotov is a Grade 11 student at Longfields-Davidson Heights Secondary School. Driven by a passion for developing projects that connect STEM and education, he has experience in nonprofit leadership, freelance software development, and organizing hackathons offering prizes totaling six figures.",
-    linkedin: "#",
+    linkedin: "https://www.linkedin.com/in/eldiiar/",
   },
   {
     name: "Kanuck Shah",
@@ -22,7 +22,7 @@ export const team: TeamMember[] = [
     role: "Chief Operations Officer",
     image: "/abt.png",
     bio: "Kanuck Shah is a Grade 11 student who loves supporting his community and bringing people together. He’s passionate about tech, leadership, and trying new things. When he’s not working on something creative, he’s playing basketball, having fun, and bringing his usual energy. He’s been involved in many ventures but always knows how to lighten the mood.",
-    linkedin: "#",
+    linkedin: "https://www.linkedin.com/in/kanuck-shah-4959922bb/",
   },
   {
     name: "Matteo Maina",
@@ -30,7 +30,7 @@ export const team: TeamMember[] = [
     role: "Chief Financial Officer",
     image: "/abt.png",
     bio: "My name is Matteo Maina, and I am currently a Grade 11 IB student at Merivale High School. Although being new to the STEM field, I have had previous leadership/management experience in overseeing non profits across business and educational fields. I am excited to see what awaits us and the impact Univa Dev will bring!",
-    linkedin: "#",
+    linkedin: "https://www.linkedin.com/in/matteo-maina-3713422ab/",
   },
 ];
 
