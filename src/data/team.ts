92f7dab4e3733b1c8e4162ae45ee5352e4e40e78
Eldiiar Bekbolotov <eldiiarbekbolotov@gmail.com>
2025-12-08 19:45:19 -0500

    .

--- src/data/team.ts
+++ src/data/team.ts
@@ -32,5 +32,46 @@ export const team: TeamMember[] = [
     bio: "My name is Matteo Maina, and I am currently a Grade 11 IB student at Merivale High School. Although being new to the STEM field, I have had previous leadership/management experience in overseeing non profits across business and educational fields. I am excited to see what awaits us and the impact Univa Dev will bring!",
     linkedin: "https://www.linkedin.com/in/matteo-maina-3713422ab/",
   },
+  {
+    name: "Anosh Dayananthan",
+    slug: "anosh-daya",
+    role: "Chief Growth Officer",
+    image: "/abt.png",
+    bio: "I’m Anosh, a Grade 10 student at Longfields-Davidson Heights Secondary School student who’s passionate about tech, leadership, and real growth. As Univa Dev’s CGO, I’m focused on expanding our community and developing meaningful opportunities. Outside the classroom, I like to code projects and I have big passion for finance and I love to invest/trade.",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    name: "Alireza Nouhehkhan",
+    slug: "alireza-nouhehkhan",
+    role: "Chief Science Officer",
+    image: "/abt.png",
+    bio: "",
+    linkedin: "https://www.linkedin.com/in/alireza-n-00a993324/",
+  },
+  {
+    name: "Chandan Nir",
+    slug: "chandan-nir",
+    role: "Chief Logistics Officer",
+    image: "/abt.png",
+    bio: "Hi, my name is Chandan, I am Grade 11 student at Longfields-Davidson Heights Secondary School. I have a passion for coding, chess and robotics! I am excited to see the future of Univa Dev!",
+    linkedin: "https://www.linkedin.com/in/chandan-nir-280a0433b/",
+  },
+  {
+    name: "Yash Trivedi",
+    slug: "yash-trivedi",
+    role: "Director of Growth",
+    image: "/abt.png",
+    bio: "",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    name: "Ryan Mah",
+    slug: "ryan-mah",
+    role: "Director of Technology",
+    image: "/abt.png",
+    bio: "Hi my name is Ryan, I’m a grade 11 student at LDHSS with a passion for coding, technology and problem solving. I love turning ideas into projects and exploring new challenges.",
+    linkedin: "https://www.linkedin.com/in/ryanmmah/",
+  },
+  
 ];
 
